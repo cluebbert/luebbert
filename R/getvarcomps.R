2df5538d586ad4a3553efa3a5269ee4f4e520256
cluebbert <cluebbert@danforthcenter.org>
2023-11-07 12:43:03 -0600

    Fix type-o in get.varcomps()

--- R/getvarcomps.R
+++ R/getvarcomps.R
@@ -1,6 +1,6 @@
 get.varcomps <- function(model.object){
   stopifnot(inherits(model.object, "lmerMod"))
-  m <- as.data.frame(lem4::VarCorr(model.object))
+  m <- as.data.frame(lme4::VarCorr(model.object))
   t <- data.frame(m$grp, pct.var = sapply(m$vcov, function(x) x / sum(m$vcov)))
   return(t)
 }
