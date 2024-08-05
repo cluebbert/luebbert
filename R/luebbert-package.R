5ff49806a81669983e9d30d259a5ec664eae4678
cluebbert <cluebbert@danforthcenter.org>
2024-08-05 17:12:21 -0500

    adding some package dependencies to documentation

---
+++ R/luebbert-package.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom grDevices dev.off
+#' @importFrom grDevices png
+#' @importFrom stats cor
+## usethis namespace: end
+NULL
