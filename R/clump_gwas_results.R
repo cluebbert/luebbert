378cb8b53b4e5c40783b0db9192c4a72e835f430
cluebbert <cluebbert@danforthcenter.org>
2024-08-19 15:48:45 -0500

    Adding clumping function.

---
+++ R/clump_gwas_results.R
@@ -0,0 +1,22 @@
+#' Title
+#'
+#' @param bed.file path to bedfile, with .bed extension, need .bim and .fam in directory too
+#' @param gwas.res.table table that contains pvalues, will be subset
+#' @param pval.column name of pvalue column
+#' @param r2thresh LD threshold for clumping
+#' @param window.size size of window for clumping
+#'
+#' @return gwas results table subsetted to include only representative snps.
+#' @export
+#'
+#' @examples
+#' # no example
+clump_gwas_results <- function(bed.file, gwas.res.table, pval.column, r2thresh = .2, window.size = 1000){
+  bed.in <- bigsnpr::bed(bed.file)
+  x <- bigsnpr::bed_clumping(obj.bed = bed.in,
+                             S = -log10(gwas.res.table[,pval.column]),
+                             thr.r2 = r2thresh,
+                             size = window.size)
+  out <- gwas.res.table[x,]
+  return(out)
+}
