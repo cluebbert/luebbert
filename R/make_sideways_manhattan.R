95e936080a9947338a3e428dc8d718893201748e
Collin Luebbert <cluebbert@danforthcenter.org>
2025-12-10 13:37:50 -0600

    Update make_sideways_manhattan.R

--- R/make_sideways_manhattan.R
+++ R/make_sideways_manhattan.R
@@ -83,7 +83,7 @@ make_sideways_manhattan <- function(gwas.res,
   # Base plot
   man <-
     ggplot(aes(x = .data$POS, y = .data$PVAL), data = plot.df) +
-    geom_point(aes(fill = .data$plot.R2, alpha = .data$how.to.plot), shape = 21, color = "black") +
+    geom_point(aes(fill = .data$plot.R2, alpha = .data$how.to.plot), size = 3, shape = 21, color = "black") +
     scale_alpha(guide = "none") +
     binned_scale(aesthetics = "fill",
                  name = "R2 \n",
